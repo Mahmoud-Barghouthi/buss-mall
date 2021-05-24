5b4ed810b1314d0e9ddcff1160ab3014ebf1f807
Mahmoud-Barghouthi <mah.barghouthi47@gmail.com>
2021-05-24 20:28:30 +0300

    last touches

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ let rightImageIndex;
 
 let allImages = [];
 
-function GetImage(name,source) {
+function GetImage(name, source) {
     this.name = name;
     this.source = source;
     this.vote = 0;
@@ -29,11 +29,12 @@ new GetImage('bathroom', 'img/bathroom.jpg');
 new GetImage('boots', 'img/boots.jpg');
 new GetImage('breakfast', 'img/breakfast.jpg');
 new GetImage('bubblegum', 'img/bubblegum.jpg');
-new GetImage('cthulhu', 'img/cthulhu.jpg');
 new GetImage('chair', 'img/chair.jpg');
+new GetImage('cthulhu', 'img/cthulhu.jpg');
+
 new GetImage('dog-duck', 'img/dog-duck.jpg');
 new GetImage('dragon', 'img/dragon.jpg');
-new GetImage('pen', 'img/bpeng.jpg');
+new GetImage('pen', 'img/pen.jpg');
 new GetImage('pet-sweep', 'img/pet-sweep.jpg');
 new GetImage('scissors', 'img/scissors.jpg');
 new GetImage('shark', 'img/shark.jpg');
@@ -43,6 +44,7 @@ new GetImage('unicorn', 'img/unicorn.jpg');
 new GetImage('water-can', 'img/water-can.jpg');
 new GetImage('wine-glass', 'img/wine-glass.jpg');
 
+console.log(allImages);
 function generateRandomIndex() {
     return Math.floor(Math.random() * allImages.length);
 }
@@ -56,11 +58,14 @@ function renderThreeImages() {
     do {
         leftImageIndex = generateRandomIndex();
         rightImageIndex = generateRandomIndex();
-    }
-    while (leftImageIndex === midImageIndex || leftImageIndex === rightImageIndex || midImageIndex === rightImageIndex);
-    leftImage.src = allImages[leftImage].source;
-    midImage.src = allImages[midImage].source;
-    rightImage.src = allImages[rightImage].source;
+    } while (leftImageIndex === midImageIndex || leftImageIndex === rightImageIndex || midImageIndex === rightImageIndex);
+
+    // leftImage.src = allImages[leftImage].source;
+    // midImage.src = allImages[midImage].source;
+    // rightImage.src = allImages[rightImage].source;
+    leftImage.src = allImages[leftImageIndex].source;
+    midImage.src = allImages[midImageIndex].source;
+    rightImage.src = allImages[rightImageIndex].source;
 
     allImages[leftImageIndex].show++;
     allImages[midImageIndex].show++;
@@ -94,6 +99,16 @@ function mouseClick(event) {
         rightImage.removeEventListener('click', mouseClick);
 
         let list = document.getElementById('list');
-        
+        let button = document.getElementById('button');
+        button.addEventListener('click', result)
+
+        function result() {
+            let liElement;
+            for (let i = 0; i < allImages.length; i++) {
+                liElement = document.createElement('li');
+                list.appendChild(liElement);
+                liElement.textContent = `${allImages[i].name} are ${allImages[i].votes} votes, and was seen ${allImages[i].show} times`
+            }
+        }
     }
 }
